b23434cae21f96428c62d4cdf8d9fcd7b86bd7a1
Federico Pedraza <federico.pedraza4@gmail.com>
2025-07-25 18:02:04 -0300

    cors 2

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.use(cors({
     'https://the-english-crab-app-git-main-federico-pedrazas-projects.vercel.app',
     'http://localhost:5173'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  optionsSuccessStatus: 200
 }));
 app.use(express.json());
 
